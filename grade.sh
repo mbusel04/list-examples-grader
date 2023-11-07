dfdca81776e93ee6b699973e7f118ff587fbd3e1
mbusel04 <mbusel@ucsd.edu>
2023-11-07 13:52:26 -0800

    Halfway I guess

--- grade.sh
+++ grade.sh
@@ -1,16 +1,22 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar"
+JUNIT_CORE='org.junit.runner.JUnitCore'
+TEST_CLASS='TestListExamples'
 
 rm -rf student-submission
 rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
+git clone $1 student-submission;
 echo 'Finished cloning'
 
+cp $TEST_CLASS.java grading-area/
+cp student-submission/ListExamples.java grading-area/
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+cd grading-area/
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+javac -cp $CPATH $TEST_CLASS.java ListExamples.java;
+# java -cp $CPATH $JUNIT_CORE $TEST_CLASS
+TEST_RESULT=`java -cp $CPATH $JUNIT_CORE $TEST_CLASS | tail -10`;
+
+echo $TEST_RESULT
