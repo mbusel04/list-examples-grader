36b48303ba54af3d98558ba512ee1c651a5ae682
mbusel04 <mbusel@ucsd.edu>
2023-11-28 13:16:54 -0800

    added autograder and 13 unit tests

--- TestListExamples.java
+++ TestListExamples.java
@@ -9,13 +9,119 @@ class IsMoon implements StringChecker {
   }
 }
 
+class Is3Letters implements StringChecker {
+  public boolean checkString(String s) {
+    return s.length() == 3;
+  }
+}
+
 public class TestListExamples {
   @Test(timeout = 500)
-  public void testMergeRightEnd() {
+  public void testMergeRightEnd1() {
+    List<String> left = Arrays.asList();
+    List<String> right = Arrays.asList();
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList();
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMergeRightEnd2() {
+    List<String> left = Arrays.asList("b");
+    List<String> right = Arrays.asList();
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("b");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMergeRightEnd3() {
+    List<String> left = Arrays.asList();
+    List<String> right = Arrays.asList("d");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("d");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMergeRightEnd4() {
     List<String> left = Arrays.asList("a", "b", "c");
     List<String> right = Arrays.asList("a", "d");
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMergeRightEnd5() {
+    List<String> left = Arrays.asList("b", "d", "f");
+    List<String> right = Arrays.asList("c");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("b", "c", "d", "f");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter1() {
+    List<String> input = Arrays.asList();
+    List<String> filtered = ListExamples.filter(input, new IsMoon());
+    List<String> expected = Arrays.asList();
+    assertEquals(expected, filtered);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter2() {
+    List<String> input = Arrays.asList("a", "b", "b");
+    List<String> filtered = ListExamples.filter(input, new IsMoon());
+    List<String> expected = Arrays.asList();
+    assertEquals(expected, filtered);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter3() {
+    List<String> input = Arrays.asList("a", "moon", "b");
+    List<String> filtered = ListExamples.filter(input, new IsMoon());
+    List<String> expected = Arrays.asList("moon");
+    assertEquals(expected, filtered);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter4() {
+    List<String> input = Arrays.asList("a", "Moon", "b", "a", "Moon", "b", "boon", "doon", "a", "moon", "b");
+    List<String> filtered = ListExamples.filter(input, new IsMoon());
+    List<String> expected = Arrays.asList("Moon", "Moon", "moon");
+    assertEquals(expected, filtered);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter5() {
+    List<String> input = Arrays.asList();
+    List<String> filtered = ListExamples.filter(input, new Is3Letters());
+    List<String> expected = Arrays.asList();
+    assertEquals(expected, filtered);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter6() {
+    List<String> input = Arrays.asList("a", "b", "c");
+    List<String> filtered = ListExamples.filter(input, new Is3Letters());
+    List<String> expected = Arrays.asList();
+    assertEquals(expected, filtered);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter7() {
+    List<String> input = Arrays.asList("12", "123", "1234", "321");
+    List<String> filtered = ListExamples.filter(input, new Is3Letters());
+    List<String> expected = Arrays.asList("123", "321");
+    assertEquals(expected, filtered);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter8() {
+    List<String> input = Arrays.asList(" ", "  ", "   ","1", "11", "111", "2", "22", "222");
+    List<String> filtered = ListExamples.filter(input, new Is3Letters());
+    List<String> expected = Arrays.asList("   ", "111", "222");
+    assertEquals(expected, filtered);
   }
 }
--- grade.sh
+++ grade.sh
@@ -2,21 +2,59 @@ CPATH=".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar"
 JUNIT_CORE='org.junit.runner.JUnitCore'
 TEST_CLASS='TestListExamples'
 
+# Clear grading areas
 rm -rf student-submission
 rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission;
-echo 'Finished cloning'
+# Clone the ropository
+git clone $1 student-submission &> grading-area/temp.txt
+if [[ $? -ne 0 ]]
+then
+    echo 'Could not find repositiory: ' $1
+    exit 1
+else
+    echo 'Finished cloning'
+fi
+
+# Copy student submission and unit tests
+STUDENT_FILE=`find student-submission -name "ListExamples.java"`
+if [[ $? -ne 0 ]]
+then
+    echo "No file named ListExamples.java in submission!"
+    exit 1
+fi
 
 cp $TEST_CLASS.java grading-area/
-cp student-submission/ListExamples.java grading-area/
+cp $STUDENT_FILE grading-area/
 
+# Start grading process
 cd grading-area/
 
-javac -cp $CPATH $TEST_CLASS.java ListExamples.java;
-# java -cp $CPATH $JUNIT_CORE $TEST_CLASS
-TEST_RESULT=`java -cp $CPATH $JUNIT_CORE $TEST_CLASS | tail -10`;
+javac -cp $CPATH $TEST_CLASS.java ListExamples.java &> compile-error.txt
 
-echo $TEST_RESULT
+# Check if programm compiles
+if [[ $? -ne 0 ]] 
+then
+    echo "Erorr during compilation, see result file for more details!"
+
+    # Write results of grading
+    echo "Compile Error: " > ../grading-result.txt
+    cat compile-error.txt >> ../grading-result.txt
+    exit 1
+fi
+
+java -cp $CPATH $JUNIT_CORE $TEST_CLASS &> test-result.txt
+
+# Check JUnit tests results
+if [[ $? -ne 0 ]]
+then
+    echo "Unit test failure, see result file for more details!"
+else
+    echo "All tests succeded! No errors found!"
+fi
+
+# Write results of grading
+echo "Test Results: " > ../grading-result.txt
+cat test-result.txt >> ../grading-result.txt
